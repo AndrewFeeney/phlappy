b57289a5b324d99bbe610ae8794504cd5ea4faf3
Andrew Feeney <andrew@deskspacecreative.com.au>
2024-11-09 08:39:44 +1000

    Add play command

---
+++ app/Console/Commands/Play.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Play extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:play';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Play flappy';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+    }
+}
