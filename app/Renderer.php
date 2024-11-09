62ecb2eddbbd9998f7c05249665aade9e3cc7473
Andrew Feeney <andrew@deskspacecreative.com.au>
2024-11-09 15:05:41 +1000

    Move phlappy to middle of page

--- app/Renderer.php
+++ app/Renderer.php
@@ -9,9 +9,15 @@ class Renderer extends DefaultRenderer
     public function __invoke(Phlappy $prompt): string
     {
         $height = $prompt->terminal()->lines();
+        $width = $prompt->terminal()->cols();
 
         foreach (range($height - 2, 0) as $row) {
-            $this->line($row === $prompt->bird()->altitude() ? $prompt->bird()->render() : ' ');
+            if ($row === $prompt->bird()->altitude()) {
+                $leftPadding = str_repeat(' ', floor(($width - 3) / 2));
+                $this->line($leftPadding . $prompt->bird()->render());
+            } else {
+                $this->line(' ');
+            }
         }
 
         return $this;
