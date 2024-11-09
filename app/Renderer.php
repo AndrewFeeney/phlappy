06b876915a2e126277d2f210d4a1d93eae709d69
Andrew Feeney <andrew@deskspacecreative.com.au>
2024-11-09 17:41:43 +1000

    Refactor: Extract method displayInfo

--- app/Renderer.php
+++ app/Renderer.php
@@ -13,15 +13,24 @@ class Renderer extends DefaultRenderer
 
         foreach (range($height - 2, 0) as $row) {
             if ($row === 0) {
-                $this->line("Altitude: {$prompt->bird()->altitude()}    Row: {$birdRow}    Rate Of Climb: {$prompt->bird()->rateOfClimb()}");
+                $this->renderInfo($prompt);
             } else if ($row === $prompt->bird()->row()) {
                 $leftPadding = str_repeat(' ', floor(($width - 3) / 2));
                 $this->line($leftPadding . $prompt->bird()->render());
             } else {
-                $this->line($row);
+                $this->line(' ');
             }
         }
 
         return $this;
+    }
+
+    private function renderInfo(Phlappy $phlappy)
+    {
+        $this->line(implode('  ', [
+            "Altitude: {$phlappy->bird()->altitude()}",
+            "Row: {$phlappy->bird()->row()}",
+            "Rate Of Climb: {$phlappy->bird()->rateOfClimb()}",
+        ]));
     }
 }
