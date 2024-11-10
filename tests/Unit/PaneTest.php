1d35f7bd1728bb0dadc58edc6857c92041cd3391
Andrew Feeney <andrew@deskspacecreative.com.au>
2024-11-10 13:39:03 +1100

    Decapitalise test names

--- tests/Unit/PaneTest.php
+++ tests/Unit/PaneTest.php
@@ -6,13 +6,13 @@ use App\Pane;
 use App\Row;
 
 describe("Pane object", function () {
-    it("Can render an empty row", function () {
+    it("can render an empty row", function () {
         $pane = new Pane(1, 1);
 
         expect($pane->renderRow(0))->toBe('');
     });
 
-    it("Can render an row with a single layer with a single cell", function () {
+    it("can render an row with a single layer with a single cell", function () {
         $cell = new Cell('*');
         $row = new Row(collect([$cell]));
         $layer = new Layer(collect([$row]));
