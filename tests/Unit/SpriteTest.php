8241ffff5ded0e97d5d314b7d3263252a55d035c
Andrew Feeney <andrew@deskspacecreative.com.au>
2024-11-18 20:47:23 +1100

    It can render multiline sprites

--- tests/Unit/SpriteTest.php
+++ tests/Unit/SpriteTest.php
@@ -22,4 +22,21 @@ describe('Render method', function () {
 
         expect($result)->toBe(['0_0']);
     });
+
+    it('can render a three x three sprite to an array of lines', function () {
+        $sprite = new Sprite();
+        $sprite->addTile(x: 0, y: 0, character: '0');
+        $sprite->addTile(x: 1, y: 0, character: '_');
+        $sprite->addTile(x: 2, y: 0, character: '0');
+        $sprite->addTile(x: 0, y: 1, character: '\\');
+        $sprite->addTile(x: 1, y: 1, character: '_');
+        $sprite->addTile(x: 2, y: 1, character: '/');
+
+        $result = $sprite->render();
+
+        expect($result)->toBe([
+            '0_0',
+            '\\_/',
+        ]);
+    });
 });
